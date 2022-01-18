76135b6bfcc18f4ff69bc7af47c7bf9f5d95ff96
0x A.K <0x.bejaxer@gmail.com>
2022-01-18 12:49:20 -0500

    build: ariva lottery

---
+++ contracts/interfaces/lottery/IRandomNumberGenerator.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IRandomNumberGenerator {
+    /**
+     * Requests randomness from a user-provided seed
+     */
+    function getRandomNumber() external;
+
+    /**
+     * View latest lotteryId numbers
+     */
+    function viewLatestLotteryId() external view returns (uint256);
+
+    /**
+     * Views random result
+     */
+    function viewRandomResult() external view returns (uint32);
+}
