531f28fd6332108e55af7effc2c5f2b4749d6026
0x A.K <37297413+bejaxer@users.noreply.github.com>
2022-01-19 11:42:13 -0500

    build: ariva lottery (#3)

---
+++ contracts/lottery/RandomNumberGenerator.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/VRFRequestIDBase.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+import "../interfaces/lottery/IRandomNumberGenerator.sol";
+import "../interfaces/lottery/IArivaLottery.sol";
+
+contract RandomNumberGenerator is VRFConsumerBase, IRandomNumberGenerator, Ownable {
+    using SafeERC20 for IERC20;
+
+    address public arivaLottery;
+    bytes32 public keyHash;
+    bytes32 public latestRequestId;
+    uint32 public randomResult;
+    uint256 public fee;
+    uint256 public latestLotteryId;
+
+    /**
+     * @notice Constructor
+     * @dev RandomNumberGenerator must be deployed before the lottery.
+     * Once the lottery contract is deployed, setLotteryAddress must be called.
+     * https://docs.chain.link/docs/vrf-contracts/
+     * @param _vrfCoordinator: address of the VRF coordinator
+     * @param _linkToken: address of the LINK token
+     */
+    constructor(address _vrfCoordinator, address _linkToken) VRFConsumerBase(_vrfCoordinator, _linkToken) {
+        //
+    }
+
+    /**
+     * @notice Request randomness from a user-provided seed
+     */
+    function getRandomNumber() external override {
+        require(msg.sender == arivaLottery, "Only ArivaLottery");
+        require(keyHash != bytes32(0), "Must have valid key hash");
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK tokens");
+
+        latestRequestId = requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * @notice Change the fee
+     * @param _fee: new fee (in LINK)
+     */
+    function setFee(uint256 _fee) external onlyOwner {
+        fee = _fee;
+    }
+
+    /**
+     * @notice Change the keyHash
+     * @param _keyHash: new keyHash
+     */
+    function setKeyHash(bytes32 _keyHash) external onlyOwner {
+        keyHash = _keyHash;
+    }
+
+    /**
+     * @notice Set the address for the ArivaLottery
+     * @param _arivaLottery: address of the PolyAriva lottery
+     */
+    function setLotteryAddress(address _arivaLottery) external onlyOwner {
+        arivaLottery = _arivaLottery;
+    }
+
+    /**
+     * @notice It allows the admin to withdraw tokens sent to the contract
+     * @param _tokenAddress: the address of the token to withdraw
+     * @param _tokenAmount: the number of token amount to withdraw
+     * @dev Only callable by owner.
+     */
+    function withdrawTokens(address _tokenAddress, uint256 _tokenAmount) external onlyOwner {
+        IERC20(_tokenAddress).safeTransfer(address(msg.sender), _tokenAmount);
+    }
+
+    /**
+     * @notice View latestLotteryId
+     */
+    function viewLatestLotteryId() external view override returns (uint256) {
+        return latestLotteryId;
+    }
+
+    /**
+     * @notice View random result
+     */
+    function viewRandomResult() external view override returns (uint32) {
+        return randomResult;
+    }
+
+    /**
+     * @notice Callback function used by ChainLink's VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        require(latestRequestId == requestId, "Wrong requestId");
+        randomResult = uint32(1000000 + (randomness % 1000000));
+        latestLotteryId = IArivaLottery(arivaLottery).viewCurrentLotteryId();
+    }
+
+    /**
+     * For TEST, should remove on mainnet
+     */
+    // function setResult(uint256 randomness) external onlyOwner {
+    //     randomResult = uint32(1000000 + (randomness % 1000000));
+    //     latestLotteryId = IArivaLottery(arivaLottery).viewCurrentLotteryId();
+    // }
+}
